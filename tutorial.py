8439ffa58b6005d1bebf1d02ee6e32581bfe39ab
Insake <Insanbeksake@gmail.com>
2021-02-12 17:14:10 +0600

    add tutorial

---
+++ tutorial.py
@@ -0,0 +1,13 @@
+def make_negative(number):
+    number = user
+    if user < 0 :
+        print(user)
+        return user
+
+    elif user >= 0:
+        a = user * -1
+        print(a)
+        return a
+
+user = int(input("Введите число:"))
+make_negative(user)
